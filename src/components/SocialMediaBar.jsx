1f9521e456ab85c545914ac5c62b0baee8d8eda2
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-09-16 04:21:33 +0530

    fix: build issue resolution

--- src/components/SocialMediaBar.jsx
+++ src/components/SocialMediaBar.jsx
@@ -6,21 +6,33 @@ import iconTwitter from "../images/icon-twitter.svg";
 export const SocialMediaBar = () => {
   return (
     <div className="flex flex-row justify-evenly pt-5 w-40">
-      <a href="https://github.com/Shivaansh-Agarwal" target="_blank">
+      <a
+        href="https://github.com/Shivaansh-Agarwal"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
           src={iconGithub}
           alt="Github"
           className="w-8 h-8 sm:w-10 sm:h-10"
         />
       </a>
-      <a href="https://www.linkedin.com/in/shivaansh-agarwal/" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/shivaansh-agarwal/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
           src={iconLinkedin}
           alt="Linkedin"
           className="w-8 h-8 sm:w-10 sm:h-10"
         />
       </a>
-      <a href="https://twitter.com/Shivansh_97" target="_blank">
+      <a
+        href="https://twitter.com/Shivansh_97"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
           src={iconTwitter}
           alt="Twitter"
